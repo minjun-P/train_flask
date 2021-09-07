23558c4b416446c01e3d1ed6243990efcbc961e7
minjun-P <jun7332568@gmail.com>
2021-09-07 17:02:19 +0900

    db고민중

--- config/development.py
+++ config/development.py
@@ -1,7 +1,7 @@
 from config.default import *
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(
-    os.path.join(BASE_DIR, "pybo.db")
+    os.path.join(BASE_DIR, "app.db")
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "dev"
